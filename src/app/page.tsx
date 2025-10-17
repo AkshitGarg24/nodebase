39255df465c245b5274de73b49ed2f18d58e61fd
Akshit Garg <akshitgarg2412@gmail.com>
2025-10-17 14:27:28 +0530

    Database Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
-const Page = () => {
-  return <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>;
+import prisma from "@/lib/db";
+
+const Page = async () => {
+  const users = await prisma.user.findMany();
+  return <div>
+    {users.map((user) => (
+      <div key={user.id}>{user.name} - {user.email}</div>
+    ))}
+  </div>;
 };
 
 export default Page;
